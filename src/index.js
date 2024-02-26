606f7b1ef18781ac8c1b2799fb089fdc4c940309
rohandatr <rohandatar2003@gmail.com>
2024-02-26 20:48:34 +0530

    database connectivity

--- src/index.js
+++ src/index.js
@@ -23,6 +23,13 @@ import express from "express"
     }catch(error){
         console.log("ERROR:", error)
         throw err
-    }
+    })
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+    })
+} catch (error){
+    console.log("Error: ", error)
+    throw err
+}
 }) ()
 */
