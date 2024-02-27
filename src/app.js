3abdc5b3e2e0be6fe0210925a922c2d8b03c0d97
rohandatr <rohandatar2003@gmail.com>
2024-02-27 17:59:15 +0530

    Api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+app.use(cors({              //app.use is for configuration || middleware
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+//request handling on website
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
 
 
 export { app }
