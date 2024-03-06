35b0889d4e868995efd12b40275d717a39f06cd4
rohandatr <rohandatar2003@gmail.com>
2024-03-06 21:13:42 +0530

    updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler } from "./utils/asyncHandler.js"
-import { ApiError } from "..utils/ApiError"
+import { ApiError } from "../ApiError.jsutils/ApiError"
+import { User } from "../models/user.model.js"
 
 const registerUser = asyncHandler( async(req, res) =>{
     //get user detailss from frontend
@@ -15,9 +16,22 @@ const registerUser = asyncHandler( async(req, res) =>{
     const {fullName, email, username, password} = req.body
     console.lof("email:" , email);
 
-    if(fullName == ""){
-        throw new ApiError(400,  "fullname is required")
+    if( [fullName, email, username, password].some((field) => field?.trim() === ""))
+    {
+        throw new ApiError(400, "All fields are required")
     }
+
+// -----------------------------------------------------
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if(existedUser){
+        throw new ApiError(409, "User with email or username existed")
+    }
+
+    
 })
 
 export { registerUser }
