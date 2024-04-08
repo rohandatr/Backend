d42ae82f36bb22eceb69b1329fb174888d699532
rohandatr <rohandatar2003@gmail.com>
2024-04-08 15:07:36 +0530

    login logut controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -119,11 +119,50 @@ const loginUser = asyncHandler( async(req, res) => {
         throw new ApiError(400,"Invalid user credentials")
     }
 
-    const {accessToken, refreshtoken}await generateAccessAndRefereshTokens(user._id)
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
 
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(200,{
+            user: loggedInUser, accessToken, refreshToken
+        },"User logged in successfully")
+    )
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logout"))
 })
 
 export { 
-    registerUser,
-    loginUser
+    registerUser, 
+    loginUser,
+    logoutUser
 } 
