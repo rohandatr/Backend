452d967bf2e4016403bbc3c23ed453840f0595f8
rohandatr <rohandatar2003@gmail.com>
2024-04-08 12:42:17 +0530

    user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,23 @@ import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
+const generateAccessAndRefereshTokens = async(userId) => 
+{
+    try {
+       const user = await User.findById(userId)
+       const accessToken = user.generateAccessToken()
+       const refreshToken = user.generateRefreshToken()
+
+       user.refreshToken = refreshToken
+       await user.save({ validateBeforeSave: false })
+
+       return {accessToken, refreshToken}
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
+    }
+}
+
 const registerUser = asyncHandler( async(req, res) =>{
     //get user detailss from frontend
     //validation - note empty
@@ -74,4 +91,39 @@ const registerUser = asyncHandler( async(req, res) =>{
     )
 }) 
 
-export { registerUser } 
+const loginUser = asyncHandler( async(req, res) => {
+    //req body -> data
+    //username or email login
+    //find the user
+    //password check
+    //access and referesh token
+    //send cookie 
+
+    const {email, username, password} = req.body
+
+    if(!username || !email){
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{username}, {email}]
+    })
+
+    if(!user){
+        throw new ApiError(400,"User does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+
+    if(!isPasswordValid){
+        throw new ApiError(400,"Invalid user credentials")
+    }
+
+    const {accessToken, refreshtoken}await generateAccessAndRefereshTokens(user._id)
+
+})
+
+export { 
+    registerUser,
+    loginUser
+} 
