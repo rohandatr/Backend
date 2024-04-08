f1086590a1af8a18598879243fc0dd598266da8b
rohandatr <rohandatar2003@gmail.com>
2024-04-08 20:09:59 +0530

    verifyJWT

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
-import { Router } from "express"
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js"
-import { upload } from "../middlewares/multer.middleware.js"
-import jwt from "jsonwebtoken"
+import { Router } from "express";
+import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
 
 const router = Router()
 
