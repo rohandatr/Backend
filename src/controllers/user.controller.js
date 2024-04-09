c689db801a34ea1462b5ee15ceccf08cc8f60ca1
rohandatr <rohandatar2003@gmail.com>
2024-04-09 17:46:05 +0530

    user model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { jwt } from "jsonwebtoken"
+import jwt  from "jsonwebtoken"
 
 const generateAccessAndRefereshTokens = async(userId) => 
 {
@@ -165,7 +165,7 @@ const logoutUser = asyncHandler(async(req, res) => {
 const refreshAccessToken = asyncHandler(async(req, res,) =>{
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if(incomingRefreshToken){
+    if(!incomingRefreshToken){
         throw new ApiError(401, "unauthorized request")
     }
 
