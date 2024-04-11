e1440544eccd7ee4e632c53088ba805923e24336
rohandatr <rohandatar2003@gmail.com>
2024-04-11 15:43:44 +0530

    controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -210,7 +210,7 @@ const refreshAccessToken = asyncHandler(async(req, res,) =>{
 const changeCurrentPassword = asyncHandler(async(req,res) => {
     const {oldPassword, newPassword} = req.body
 
-    const user = await User.findById(req.user?.id)
+    const user = await User.findById(req.user?._id)
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
     if(!isPasswordCorrect){
@@ -224,9 +224,36 @@ const changeCurrentPassword = asyncHandler(async(req,res) => {
     .json(new ApiResponse(200, {}, "Password changed successfully"))
 })
 
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(200, req.user, "current user fetched successfully")
+})
+
+const updateAccountDetails = asyncHandler(async(req,res) => {
+    const {fullName, email} = req.body
+
+    if(!fullName || !email){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullName,
+                email
+            }
+        },
+        {new: true}
+    ).select("-password")
+})
+
 export { 
     registerUser, 
     loginUser,
     logoutUser,
-    refreshAccessToken
-} 
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser
+}
